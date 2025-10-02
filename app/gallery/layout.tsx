e66b13902a857fa4d817b8317a2326933563be2d
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2025-10-02 19:09:11 +0300

    Create layout.tsx in gallery page

---
+++ app/gallery/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Gallery - @SDAMTC',
+}
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
